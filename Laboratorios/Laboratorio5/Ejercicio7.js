5a2f1f87aa7ba9fcdb0cbd51c29eb23e2aa230f9
joseg23 <00051716@uca.edu.sv>
2019-09-27 10:38:54 -0600

    Ejercicio 7

---
+++ Laboratorios/Laboratorio5/Ejercicio7.js
@@ -0,0 +1,18 @@
+/* Fibonacci: La serie Fibonacci es una serie matemática muy reconocida, la cual tiene como primeros 10 términos: 0, 1, 1, 2, 3, 5, 8, 13, 21, 34. 
+Realice una función en JavaScript que reciba un número n como argumento y muestre la serie hasta el enésimo término.  */
+function fibonacci(n) {
+    let first=1;
+    let second=2;
+  
+    for (let index=3; index<n; index++) {
+      let temp=first+second;
+      console.log(temp);
+      first=second;
+      second=temp;
+    }
+  
+    if (n<=2)
+      return 1;
+  
+    return first+second;
+  }
