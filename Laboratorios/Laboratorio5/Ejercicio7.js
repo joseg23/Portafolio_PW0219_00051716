152e455c7f66cf21f33674b41804a724a8f0242c
joseg23 <00051716@uca.edu.sv>
2019-09-27 10:42:39 -0600

    Ejercicio 8

--- Laboratorios/Laboratorio5/Ejercicio7.js
+++ Laboratorios/Laboratorio5/Ejercicio7.js
@@ -16,3 +16,4 @@ function fibonacci(n) {
   
     return first+second;
   }
+
---
+++ Laboratorios/Laboratorio5/Ejercicio8.js
@@ -0,0 +1,7 @@
+/*Conversor decimal-binario: Crear una función en JavaScript, que reciba como parámetro un entero positivo.
+ La función debe devolver el valor en binario.  */
+function bin(num){
+    console.log(num.toString(2));
+}
+
+bin(23);
